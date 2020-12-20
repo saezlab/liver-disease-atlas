6c883023f16abdeef776416ccb61382e9ff24b0d
christianholland <holland-2017>
2020-12-20 18:13:53 +0100

    style formatting

--- code/human-hoang-nafld/preprocessing.R
+++ code/human-hoang-nafld/preprocessing.R
@@ -5,44 +5,48 @@ library(GEOquery)
 source(here("code/utils-utils.R"))
 
 # Download meta data ------------------------------------------------------
-df = getGEO("GSE130970")
+df <- getGEO("GSE130970")
 
-meta_data = df$GSE130970_series_matrix.txt.gz %>%
+meta_data <- df$GSE130970_series_matrix.txt.gz %>%
   pData() %>%
   rownames_to_column("sample") %>%
   as_tibble() %>%
-  select(sample = title, fibrosis = `fibrosis stage:ch1`,
-         lobular_inflammation = `lobular inflammation grade:ch1`,
-         nafld = `nafld activity score:ch1`, gender = `Sex:ch1`,
-         steatosis = `steatosis grade:ch1`, age = `age at biopsy:ch1`,
-         ballooning = `cytological ballooning grade:ch1`) %>%
-  mutate(#sample = fct_inorder(sample),
+  select(
+    sample = title, fibrosis = `fibrosis stage:ch1`,
+    lobular_inflammation = `lobular inflammation grade:ch1`,
+    nafld = `nafld activity score:ch1`, gender = `Sex:ch1`,
+    steatosis = `steatosis grade:ch1`, age = `age at biopsy:ch1`,
+    ballooning = `cytological ballooning grade:ch1`
+  ) %>%
+  mutate( # sample = fct_inorder(sample),
     fibrosis = fct_inseq(fibrosis),
     lobular_inflammation = fct_inseq(lobular_inflammation),
     nafld = fct_inseq(nafld),
     gender = as_factor(str_to_lower(gender)),
     steatosis = fct_inseq(steatosis),
     age = as.numeric(age),
-    ballooning = fct_inseq(ballooning)) %>%
-  mutate(nafld = factor(str_c("stage", nafld, sep="_"),
-                        levels = str_c("stage", c(0:6), sep = "_")))
+    ballooning = fct_inseq(ballooning)
+  ) %>%
+  mutate(nafld = factor(str_c("stage", nafld, sep = "_"),
+    levels = str_c("stage", c(0:6), sep = "_")
+  ))
 
 
 
 # Load count matrix -------------------------------------------------------
 # read count data with entrez gene ids
-count_mat_entrez = read_csv(
+count_mat_entrez <- read_csv(
   here("data/human-hoang-nafld/GSE130970_all_sample_salmon_tximport_counts_entrez_gene_ID.csv")
-  )
+)
 
 # translate gene entrez ids to hgnc symbols
-count_mat_hgnc = count_mat_entrez %>%
+count_mat_hgnc <- count_mat_entrez %>%
   mutate(entrez_id = as.character(entrez_id)) %>%
   rename(gene = entrez_id) %>%
   translate_gene_ids(from = "entrez_hgnc", to = "symbol_hgnc")
 
 # summarize counts for duplicated genes (takes quite long)
-count_mat = count_mat_hgnc %>%
+count_mat <- count_mat_hgnc %>%
   drop_na(gene) %>%
   group_by(gene) %>%
   summarise(across(where(is.numeric), median)) %>%
@@ -50,8 +54,6 @@ count_mat = count_mat_hgnc %>%
 
 # save meta data and count matrix -----------------------------------------
 stopifnot(meta_data$sample == colnames(count_mat))
-
-saveRDS(meta, here("data/human-hoang-nafld/meta_data.rds"))
 
 saveRDS(meta_data, here("data/human-hoang-nafld/meta_data.rds"))
 saveRDS(count_mat, here("data/human-hoang-nafld/count_matrix.rds"))
